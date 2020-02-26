ed626d895114bcca9de64c995507064796d37ff7
atsushi <m01183074f@edu.teu.ac.jp>
2020-02-26 21:02:48 +0900

    コストの実装

---
+++ Assets/ActionCost.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ActionCost : MonoBehaviour
+{
+    public int DefaltMoveCost;
+    public int DefaltCanonCost;
+    public int DefaltCampCost;
+}
--- Assets/ActiveAction.cs
+++ Assets/ActiveAction.cs
@@ -7,10 +7,15 @@ public enum ActionType
 {
     None,
     Move,
-    SetObj
+    SetCanon,
+    SetCamp
 }
 public class ActiveAction : MonoBehaviour
 {
+    // ゲームコントローラー
+    private GameController controller;
+    // コスト
+    private ActionCost cost;
     // 現在選択されている状態
     public ActionType currentType;
     // クリックした場所
@@ -21,11 +26,16 @@ public class ActiveAction : MonoBehaviour
     private PlayerMove playerMove;
     // レイの距離
     private float rayRange = 100f;
+
     [SerializeField, Tooltip("大砲のプレファブ")]
     private GameObject Canon;
+    [SerializeField, Tooltip("キャンプのプレファブ")]
+    private GameObject Camp;
     // Start is called before the first frame update
     void Start()
     {
+        cost = GetComponent<ActionCost>();
+        controller = GetComponent<GameController>();
         playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
         currentType = ActionType.None;
     }
@@ -41,27 +51,49 @@ public class ActiveAction : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             // クリックした位置の取得
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, rayRange, LayerMask.GetMask("Field")))
-            {
-                clickPosition = hit.point;
-            }
-            // y軸修正
-            SetPosition = new Vector3(clickPosition.x, 0.5f, clickPosition.z);
-
+            GetClickPosition();
             // アクション処理
+            // 移動
             if (currentType == ActionType.Move)
             {
-                playerMove.MovePlayer(SetPosition);
+                if (controller.CurrentSoldiorNum > cost.DefaltMoveCost)
+                {
+                    controller.CurrentSoldiorNum -= cost.DefaltMoveCost;
+                    playerMove.MovePlayer(SetPosition);
+                }
             }
-            if (currentType == ActionType.SetObj)
+            // 大砲
+            if (currentType == ActionType.SetCanon)
             {
-                Instantiate(Canon, SetPosition, Quaternion.identity);
+                if (controller.CurrentSoldiorNum > cost.DefaltCanonCost)
+                {
+                    controller.CurrentSoldiorNum -= cost.DefaltCanonCost;
+                    Instantiate(Canon, SetPosition, Quaternion.identity);
+                }
+            }
+            // キャンプ
+            if (currentType == ActionType.SetCamp)
+            {
+                if (controller.CurrentSoldiorNum > cost.DefaltCampCost)
+                {
+                    controller.CurrentSoldiorNum -= cost.DefaltCampCost;
+                    Instantiate(Camp, SetPosition, Quaternion.identity);
+                }
             }
             // アクションタイプを未選択に
             currentType = ActionType.None;
         }
+    }
+    private void GetClickPosition() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayRange, LayerMask.GetMask("Field")))
+        {
+            clickPosition = hit.point;
+        }
+        // y軸修正
+        SetPosition = new Vector3(clickPosition.x, 0.5f, clickPosition.z);
+
     }
     public void SetType(int setType)
     {
