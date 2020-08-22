88b75b0110b9b684061f2db7d41f0ba865a2cef2
atsushi <m01183074f@edu.teu.ac.jp>
2020-08-22 15:52:50 +0900

    UI実装まで

--- Assets/UIReinforcedPointNumText.cs
+++ Assets/UIReinforcedPointNumText.cs
@@ -7,22 +7,47 @@ public class UIReinforcedPointNumText : MonoBehaviour
 {
     private GameController Controller;
     private Text text;
-
+    [SerializeField]
+    private GameObject[] gagechild;
+    private bool is_Change = false;
+    private int repoint;
+    private int num;
     // Start is called before the first frame update
     void Start()
     {
         Controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         text = GetComponent<Text>();
         SetText();
+        repoint = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SetText();
+        if (repoint != Controller.ReinforcedPoint)
+        {
+            SetText();
+        }
+        repoint = Controller.ReinforcedPoint;
     }
     private void SetText()
     {
         text.text = Controller.ReinforcedPoint.ToString();
+        for (int i = 0; i < gagechild.Length; i++)
+        {
+            if (i < Controller.ReinforcedPoint)
+            {
+                if (gagechild[i].activeSelf == false)
+                {
+                    gagechild[i].SetActive(true);
+                }
+            }
+            else {
+                if (gagechild[i].activeSelf == true)
+                {
+                    gagechild[i].SetActive(false);
+                }
+            }
+        }
     }
 }
