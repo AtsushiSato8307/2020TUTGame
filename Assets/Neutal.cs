87b08207c9f056a1e07c3dc4f8cd73b30a10bcc7
atsushi <m01183074f@edu.teu.ac.jp>
2020-08-30 17:25:43 +0900

    中立実装

---
+++ Assets/Neutal.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neutal : MonoBehaviour
+{
+    [SerializeField, Tooltip("敵用施設")]
+    private GameObject enemy_building;
+    [SerializeField, Tooltip("味方用施設")]
+    private GameObject player_building;
+
+    // HP
+    private HitPoint playerBuildingHp;
+    private HitPoint EnemyBuildingHp;
+    
+    // CurrentHP
+    private int enemyBuild_currentHp { set { playerBuildingHp.currentHitPoint = value; } get { return playerBuildingHp.currentHitPoint; } }
+    private int playerBuild_currentHp { set { EnemyBuildingHp.currentHitPoint = value; } get { return EnemyBuildingHp.currentHitPoint; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBuildingHp = player_building.GetComponent<HitPoint>();
+        EnemyBuildingHp = enemy_building.GetComponent<HitPoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyBuild_currentHp <= 0)
+        {
+            Fall();
+            enemy_building.GetComponent<NeutralOneSide>().buildingIntaractive = true;
+            player_building.GetComponent<NeutralOneSide>().buildingIntaractive = false;
+        }
+        else if (playerBuild_currentHp <= 0)
+        {
+            Fall();
+            enemy_building.GetComponent<NeutralOneSide>().buildingIntaractive = false;
+            player_building.GetComponent<NeutralOneSide>().buildingIntaractive = true;
+        }
+    }
+    // どちらか陥落時処理
+    private void Fall()
+    {
+        enemyBuild_currentHp = playerBuildingHp.maxHitPoint;
+        playerBuild_currentHp = EnemyBuildingHp.maxHitPoint;
+    }
+}
